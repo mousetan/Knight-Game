daae3ac0ae8bb71b81aab1482ccf08d1b8f70301
angelo <angelo.marney@gmail.com>
2022-10-03 13:40:32 -0500

    reorganized folders, added InputEventChannel.cs, added GameControls.cs

---
+++ Assets/Code/ScriptableObjects/InputEventChannel.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+/// <summary>
+/// Within a script, reference an instance of this Scriptable Object as an asset in order to give
+/// that script access to the user's inputs.
+/// </summary>
+public class InputEventChannel : ScriptableObject, GameControls.IGameplayActions
+{
+    public event UnityAction attackInputEvent = delegate { };
+    public event UnityAction jumpInputEvent = delegate { };
+    public event UnityAction<Vector2> lookInputEvent = delegate { };
+    public event UnityAction<Vector2> moveInputEvent = delegate { };
+
+    private GameControls _gameControls;
+
+    private void OnEnable()
+    {
+        if (_gameControls == null)
+        {
+            _gameControls = new GameControls();
+            //_gameControls.Dialogue.SetCallbacks(this);
+            //_gameControls.Menu.SetCallbacks(this);
+            _gameControls.Gameplay.SetCallbacks(this);
+        }
+        EnableGameplayInputs();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+            attackInputEvent?.Invoke();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+            jumpInputEvent?.Invoke();
+    }
+
+    public void OnLook(InputAction.CallbackContext context) 
+        => lookInputEvent?.Invoke(context.ReadValue<Vector2>());
+
+    public void OnMove(InputAction.CallbackContext context)
+        => moveInputEvent?.Invoke(context.ReadValue<Vector2>());
+
+    public void EnableGameplayInputs()
+    {
+        //_gameControls.Dialogue.Disable();
+        _gameControls.Gameplay.Enable();
+        //_gameControls.Menu.Disable();
+    }
+}
